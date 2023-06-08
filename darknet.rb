34f18d022b3b57a8c3f65c72de140f5728277398
Giovanni Merlino <gmerlino@unime.it>
2023-06-08 23:50:06 +0200

    Update darknet.rb

--- darknet.rb
+++ darknet.rb
@@ -1,9 +1,9 @@
 class Darknet < Formula
   desc "Convolutional Neural Networks"
-  homepage "http://pjreddie.com/darknet/"
-  head "https://github.com/pjreddie/darknet.git"
+  homepage "https://github.com/AlexeyAB/darknet"
+  head "https://github.com/AlexeyAB/darknet.git"
 
-  depends_on "opencv@2" => :build
+  depends_on "opencv@4" => :build
   depends_on "pkg-config"
 
   def install
